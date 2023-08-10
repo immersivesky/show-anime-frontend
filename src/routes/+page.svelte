7ef2f43acb9e1c61f2b2da03ba78c3ff0192f42e
immersivesky <diamondcaspersky@gmail.com>
2023-08-10 20:06:38 +0300

    Init create-svelte application

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+	<title>Home Page | Show Anime</title>
+	<meta name="description" content="Show Anime" />
+</svelte:head>
+
+<div>
+	<img
+		alt="Hell's Paradise"
+		src="https://upload.wikimedia.org/wikipedia/en/4/4e/Jigokuraku_Vol_1.jpg"
+	/>
+</div>
+
+<style>
+	div {
+		margin-top: 24px;
+		justify-content: center;
+		display: flex;
+	}
+</style>

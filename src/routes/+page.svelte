019696e7c2ed16615211213452dd865a0d9e886b
immersivesky <diamondcaspersky@gmail.com>
2023-08-16 15:21:32 +0300

    💄 Add Montserrat font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,10 @@
-<script>
-	import '../app.css'
-</script>
-
 <svelte:head>
 	<title>Home Page | Show Anime</title>
 	<meta name="description" content="Show Anime" />
 </svelte:head>
 
 <div>
-	<img
-		alt="Hell's Paradise"
-		src="https://upload.wikimedia.org/wikipedia/en/4/4e/Jigokuraku_Vol_1.jpg"
-	/>
-	<span class="text-4xl">Hell's Paradise</span>
+	<a href="/anime">
+		<p>Hell's Paradise</p>
+	</a>
 </div>
---
+++ src/routes/anime/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Hell's Paradise | Show Anime</title>
+	<meta name="description" content="Show Anime" />
+</svelte:head>
+
+<div>
+	<a href="/">
+		<p>Menu</p>
+    </a>
+
+    <img
+		alt="Hell's Paradise"
+		src="https://upload.wikimedia.org/wikipedia/en/4/4e/Jigokuraku_Vol_1.jpg"
+	/>
+	<h1 class="text-4xl">Hell's Paradise</h1>
+	<p class="text-1xl">Времена правления династии Токугава. Один из самых ловких нинздя и печально известный наемный убийца попадает в плен, но у него появляется шанс избежать казни. Для этого ему нужно отправиться на окутанный легендами загадочный остров и найти там эликсир бессмертия.	</p>
+</div>
